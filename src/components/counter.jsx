a4700f35e149923c1736c61423f127c861fd4e48
alxayeed <alxayeed@gmail.com>
2020-07-29 17:44:00 +0600

    decrement button with dynamic state changes added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,60 +1,111 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag 1", "tag 2", "tag 3"],
+  };
 
-    state = {
-        count: 0,
-        tags: ['tag 1', 'tag 2', 'tag 3']
-    };
+  style = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
 
-    style = {
-        fontSize: 20,
-        fontWeight: "bold",
+  // constructor() {
+  //     super()
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h4 style={{ backgroundColor: "#aaa", textAlign: "center" }}>
+          Hello Al!
+        </h4>
+
+        <span style={this.style} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+
+        <button
+          onClick={this.handleIncrement}
+          className={this.getIncrementButtonclasses()}
+        >
+          Increment
+        </button>
+
+        <button
+          onClick={this.handleDecrement}
+          className={this.getDecrementButtonclasses()}
+        >
+          Decrement
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  // Dynamic styling
+  getBadgeClasses() {
+    let classes = "badge m-1 badge-";
+
+    if (this.state.count < 3) {
+      classes += "danger";
+    } else if (this.state.count < 5) {
+      classes += "warning";
+    } else if (this.state.count < 10) {
+      classes += "info";
+    } else {
+      classes += "success";
+    }
+    // classes += (this.state.count < 5) ? "warning" : "success";
+    return classes;
+  }
+
+  getIncrementButtonclasses() {
+    let classes = "btn btn-sm m-1 btn-";
+
+    if (this.state.count < 3) {
+      classes += "danger";
+    } else if (this.state.count < 5) {
+      classes += "warning";
+    } else if (this.state.count < 10) {
+      classes += "info";
+    } else {
+      classes += "success";
     }
 
-    // constructor() {
-    //     super()
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
+    return classes;
+  }
+    
+  getDecrementButtonclasses() {
+    let classes = "btn btn-sm m-1 btn-";
 
-    handleIncrement = () =>  {
-        this.setState({ count: this.state.count + 1 })
+    if (this.state.count < 3) {
+      classes += "success";
+    } else if (this.state.count < 5) {
+      classes += "info";
+    } else if (this.state.count < 10) {
+      classes += "warning";
+    } else {
+      classes += "danger";
     }
 
-    render() {
-        return (
-            <React.Fragment>
-                <h4 style={{ backgroundColor: '#aaa', textAlign: "center" }}>Hello Al!</h4>
-
-                <span style={this.style} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-        </React.Fragment>
-        );
+    return classes;
     }
 
-    // Dynamic styling
-    getBadgeClasses() {
-        let classes = "badge m-3 badge-";
 
-        if (this.state.count === 0) {
-            classes += "danger"
-          } else if (this.state.count < 5) {
-            classes += "warning"
-          } else if (this.state.count <10) {
-            classes += "info"
-        }
-        else {
-            classes += "success"
-        }
-        // classes += (this.state.count < 5) ? "warning" : "success";
-        return classes;
-    }
-
-    formatCount() {
-        const { count } = this.state
-        return count === 0 ? 'Zero ' : count;
-    }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 0 : count;
+  }
 }
 
 export default Counter;
