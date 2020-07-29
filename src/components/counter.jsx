b93528a84d044c56ceb82a6281c837f7e3524f07
alxayeed <alxayeed@gmail.com>
2020-07-29 14:54:27 +0600

    adding styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Hello Al!</h1>
-        <button>Increment</button>
-      </React.Fragment>
-    );
-  }
+
+    state = {
+        count: 5,
+    };
+
+    style = {
+        fontSize: 20,
+        fontWeight: "bold",
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {/* style 1-as a variable */}
+                <h4 style={{ backgroundColor: '#aaa', textAlign: "center" }}>Hello Al!</h4>
+
+                {/* style 2 - as property */}
+                <span style={this.style} className="badge badge-success m-3">{this.formatCount()}</span>
+                
+                {/* style 3 - as a class (RECOMMENDED) */}
+            <button className="btn btn-secondary btn-sm">Increment</button>
+        </React.Fragment>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state
+        return count === 0 ? 'Zero ' : count;
+    }
 }
 
 export default Counter;
