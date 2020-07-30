08d7c767e3a6970bd799c7c269fb398c0f287732
alxayeed <alxayeed@gmail.com>
2020-07-30 16:51:54 +0600

    object destructuring

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,20 +3,19 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    const { onReset, onIncrement, onDelete } = this.props;
+
     return (
       <React.Fragment>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm n-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm n-2">
           Reset
         </button>
         {/* array of 4 components */}
         {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 //stateless Fucntional Component
 
 // snipet - sfc
-const navBar = (props) => {
+const navBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <span className="navbar-brand mb-0 h1">{props.totalCounters}</span>
+      <span className="navbar-brand mb-0 h1">{totalCounters}</span>
     </nav>
   );
 };
