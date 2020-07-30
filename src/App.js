2c8862538da4bd7d0cd0d5380d2b8091c6fec2e8
alxayeed <alxayeed@gmail.com>
2020-07-30 16:35:15 +0600

    lifting up state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./components/navbar";
 import "./App.css";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <React.Fragment>
-      <h4 style={{ backgroundColor: "#aaa", textAlign: "center" }}>
-        Hello Al!
-      </h4>
-      <Navbar />
-      <main className="container"></main>
-      <Counters />
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  titleStyle = {
+    backgroundColor: "#aaa",
+    textAlign: "center",
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    console.log(index);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h4 style={{ backgroundColor: "#aaa", textAlign: "center" }}>
+          Hello Al!
+        </h4>
+        <Navbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container"></main>
+        <Counters
+          counters={this.state.counters}
+          onReset={this.handleReset}
+          onIncrement={this.handleIncrement}
+          onDelete={this.handleDelete}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
